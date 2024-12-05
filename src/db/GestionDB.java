9ee050f5f8ad97e575cdba0fa9bf0efc082aa6b9
Tresdese <super_diegobros@hotmail.com>
2024-12-05 11:27:22 -0600

    no me acuerdo q cambie

--- src/db/GestionDB.java
+++ src/db/GestionDB.java
@@ -5,48 +5,35 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.sql.ResultSet;
 
 public class GestionDB {
-   // Conexión a la base de datos
-   public static Connection ConectarBD(String bd) {
-       Connection conexion;
-       String host = "jdbc:mysql://localhost/";
-       String user = "root";
-       String pass = "123456";
 
-       System.out.println("Conectando a la base de datos...");
-
-       try {
-           conexion = DriverManager.getConnection(host + bd, user, pass);
-           System.out.println("Conexión exitosa");
-       } catch (SQLException e) {
-           System.out.println(e.getMessage());
-           throw new RuntimeException(e);
-       }
-
-       return conexion;
-   }
-
-   public static String consultarPalabra() {
-       Connection conexion = ConectarBD("wordlechafa");
-
-       String query = "SELECT * FROM palabras";
-
-       try {
-           Statement stmt = conexion.createStatement();
-           ResultSet rs = stmt.executeQuery(query);
-           while (rs.next()) {
-               String palabra = rs.getString("palabra");
-            //    System.out.println(rs.getString("palabra"));
-               return palabra;
-           }
-           
-        } catch (SQLException e) {
-           System.out.println(e.getMessage());
-           throw new RuntimeException(e);
-        }
+    public static List<String> consultarPalabra() {
+        String query = "SELECT * FROM palabras";
         
-        return null; // Return null if no words are found
-   }
+        ConexionBD connect = new ConexionBD();
+        Connection conexion = connect.ConectarBD("wordlechafa");
+
+        List<String> palabras = new ArrayList<String>();
+
+        try {
+            Statement stmt = conexion.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                String palabra = rs.getString("palabra");
+                palabras.add(palabra);
+                //    System.out.println(rs.getString("palabra"));
+            }
+
+            return palabras;
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
 }
+
