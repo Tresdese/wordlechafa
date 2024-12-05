cd982e74f6e943e7a383cc0c0d3c9aee51062a66
ByTheAngel11 <arangeldejesus@hotmail.com>
2024-12-05 00:59:53 -0600

    MENSAJE DE AYUDA

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         // Conexion a la base de datos
 
-
+        System.out.println("HOla DIEGO DIME QUE FINCIONO PLS");
         // interfaz gracias, pero no me salio la interfaz JAJAJA
         String palabraAdivinar = "apple"; // Ejemplo de palabra a adivinar
         SwingUtilities.invokeLater(() -> new Juego(palabraAdivinar));
